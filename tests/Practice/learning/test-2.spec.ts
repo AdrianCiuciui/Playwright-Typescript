23ad445086afac5eedb2d33f727b4b816d0bbecf
Ciuciui Adrian <adrian.ciuciui.cornel@gmail.com>
2023-11-09 13:50:57 +0200

    commented out practice tests

--- tests/Practice/learning/test-2.spec.ts
+++ tests/Practice/learning/test-2.spec.ts
@@ -1,15 +1,15 @@
-import { test, expect } from '@playwright/test';
+// import { test, expect } from '@playwright/test';
 
-test('test', async ({ page }) => {
-  await page.goto('https://www.google.com/');
-  await page.getByRole('button', { name: 'Acceptă tot' }).click();
-  await page.getByLabel('Caută', { exact: true }).click();
-  await page.getByLabel('Caută', { exact: true }).fill('playwright');
-  await page.getByLabel('Căutare Google').first().click();
+// test('test', async ({ page }) => {
+//   await page.goto('https://www.google.com/');
+//   await page.getByRole('button', { name: 'Acceptă tot' }).click();
+//   await page.getByLabel('Caută', { exact: true }).click();
+//   await page.getByLabel('Caută', { exact: true }).fill('playwright');
+//   await page.getByLabel('Căutare Google').first().click();
   
-  await page.goto('https://www.google.com/search?q=playwright');
-  await page.getByRole('link', { name: 'Change to English' }).click();
-  await page.getByRole('link', { name: 'Playwright: Fast and reliable end-to-end testing for modern ... Playwright https://playwright.dev' }).click();
+//   await page.goto('https://www.google.com/search?q=playwright');
+//   await page.getByRole('link', { name: 'Change to English' }).click();
+//   await page.getByRole('link', { name: 'Playwright: Fast and reliable end-to-end testing for modern ... Playwright https://playwright.dev' }).click();
 
 
-});
+// });
