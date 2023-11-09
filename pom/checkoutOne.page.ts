95935f8e984128b59cfd786fe403c1a957b3c908
Ciuciui Adrian <adrian.ciuciui.cornel@gmail.com>
2023-11-09 15:36:08 +0200

    updated variable names

--- pom/checkoutOne.page.ts
+++ pom/checkoutOne.page.ts
@@ -13,9 +13,9 @@ export default class CheckoutOnePage {
     inputPostalCode = () => this.page.locator("#postal-code");
     buttonContinue = () => this.page.locator("#continue");
 
-    public async inputMandatoryValues(fn: string, ln: string, zip: string) {
-        await this.inputFirstName().fill(fn);
-        await this.inputLastName().fill(ln);
-        await this.inputPostalCode().fill(zip);
+    public async inputMandatoryValues(firstName: string, lastName: string, zipCode: string) {
+        await this.inputFirstName().fill(firstName);
+        await this.inputLastName().fill(lastName);
+        await this.inputPostalCode().fill(zipCode);
     }
 }
